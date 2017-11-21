43ab785b927beb7d298ddb88b794d4ac6b61cbbc
Antoine Fontaine <antoine.fontaine@dnd.fr>
2017-11-21 16:51:41 +0100

    #integration : global changes

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@
 $(document).ready(function(){
     toggleMenu('.btn-navigation', '.navigation');
     toggleMenu('.toggle-sidebar', '.sidebar-wrapper');
+    selectImport('#import-type');
+    selectImport('#file-to-import');
+    launchTerminal('#pimgento-import', '#launch-import','#import-type','#file-to-import');
 });
 
 function toggleMenu(button, menu) {
@@ -19,4 +22,50 @@ function toggleMenu(button, menu) {
             $('body').toggleClass('nav-open');
         }
     });
+}
+
+function selectImport(container) {
+    var selectContainer = $(container);
+    var selectItems = selectContainer.find('.type');
+
+    selectItems.click(function() {
+        if (!$(this).hasClass('active')) {
+            selectItems.each(function() {
+               $(this).removeClass('active');
+            });
+
+            $(this).addClass('active');
+        }
+    });
+}
+
+function launchTerminal(id, button, importContainer, fileContainer) {
+    if (button && importContainer && fileContainer) {
+            $(button).click(function(e) {
+                e.preventDefault();
+
+                var importSelected = $(importContainer).find('.type.active');
+                var fileSelected = $(fileContainer).find('.type.active');
+
+                var importSelectedDataId = importSelected.attr('data-id');
+                var fileSelectedDataId = fileSelected.attr('data-id');
+                var fileSelectedTitle = fileSelected.attr('data-title');
+
+                var dt = new Date();
+                var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+
+                if (importSelectedDataId === fileSelectedDataId) {
+                    $(id).html("<span data-ty='progress' data-ty data-ty-prompt='>' data-ty-progressChar='.'></span><span data-ty data-ty data-ty-prompt='>'>["+ time +"] You successfully import "+ fileSelectedTitle + "</span>");
+                }
+
+                else {
+                    $(id).html("<span data-ty data-ty data-ty-prompt='>'>["+ time +"] Error ! Please selected the right file.</span>");
+                }
+
+                new Termynal(id);
+            });
+
+    } else {
+        new Termynal(id);
+    }
 }
