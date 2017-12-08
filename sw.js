463b51d824123a84a97f3476e1ff4264b34f6b11
Antoine Fontaine <antoine.fontaine@dnd.fr>
2017-12-08 17:58:23 +0100

    #performance : optimize performance for mobile device

---
+++ sw.js
@@ -0,0 +1,24 @@
+importScripts('js/vendor/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('pimgento').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/index.html',
+                '/css/style.css',
+                '/js/main.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+
+        })
+    );
+});
