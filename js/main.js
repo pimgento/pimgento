c6721bfdc736ec9eec5ea55d55f9226c606705d7
Antoine Fontaine <antoine.fontaine@dnd.fr>
2018-03-06 11:09:43 +0100

    #search : add console log

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
     selectImport('#file-to-import');
     launchTerminal('#pimgento-import', '#launch-import','#import-type','#file-to-import');
     launchLazyload('img');
+    console.log('Test')
 });
 
 function toggleMenu(button, menu) {
