7a80e7c03ebcb38ca175d15d273ede9f340fe6b1
Antoine Fontaine <32138314+antoine-fontaine@users.noreply.github.com>
2018-07-17 15:23:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+importScripts('js/vendor/cache-polyfill.js');
 
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('pimgento').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/style.css',
+                '/js/main.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+
+        })
+    );
+});
